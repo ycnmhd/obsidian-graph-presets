b7ce148c4d0eccf471a3b7e39426aaf7fe9a0128
Y <33285087+ycnmhd@users.noreply.github.com>
2024-03-15 21:53:54 +0100

    fix: cannot create new presets (#18)

--- src/helpers/obsidian/graph/helpers/get-graph-leaf/get-graph-leaf.ts
+++ src/helpers/obsidian/graph/helpers/get-graph-leaf/get-graph-leaf.ts
@@ -3,15 +3,19 @@ import { GetPresetDTO } from "src/helpers/get-preset";
 import { getSnapshot } from "src/store/store";
 import { getOpenLocalGraphLeaf } from "./helpers/get-open-local-graph-leaf";
 import { getOpenGraphLeaf } from "./helpers/get-open-graph-leaf";
+import { MarkdownPresetMeta } from "src/graph-presets";
 
 export const getGraphLeaf = async (dto: GetPresetDTO) => {
 	let leaf: WorkspaceLeaf | undefined;
 	const store = getSnapshot();
-	const presetMeta = store.presets.meta[dto?.created];
+	const presetMeta = store.presets.meta[dto?.created] as
+		| MarkdownPresetMeta
+		| undefined;
+
 	const localGraphFile = dto ? presetMeta?.localGraphFile : undefined;
 	if (localGraphFile) {
 		leaf = getOpenLocalGraphLeaf(localGraphFile);
-	} else if (presetMeta.target === "local") {
+	} else if (presetMeta && presetMeta.target === "local") {
 		const file = app.workspace.getActiveFile();
 		if (file) leaf = getOpenLocalGraphLeaf(file?.stat.ctime);
 	} else {
